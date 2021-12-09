b87f9ecfbcaf4be0af1bc9e36a791856fc6e574b
Kim Göransson <kim.goransson@student.kyh.se>
2021-12-09 22:30:01 +0100

    Testing day 5

--- aoc/day5.py
+++ aoc/day5.py
@@ -24,6 +24,46 @@ class NodeLine:
         return self.vals
 
 
+def populate_grid(node_lines, grid, w, h):
+    for node in node_lines:
+        if node.vals[0] == node.vals[2]:
+            if node.vals[1] < node.vals[3]:
+                index = node.vals[3]
+                while index >= node.vals[1]:
+                    val = int(grid[index][node.vals[0]])
+                    val += 1
+                    grid[index][node.vals[0]] = val
+                    index -= 1
+
+            else:
+                index = node.vals[1]
+                while index <= node.vals[3]:
+                    val = int(grid[index][node.vals[0]])
+                    val += 1
+                    grid[index][node.vals[0]] = val
+                    index += 1
+
+        elif node.vals[1] == node.vals[3]:
+            if node.vals[0] < node.vals[2]:
+                index = node.vals[2]
+                while index >= node.vals[0]:
+                    val = int(grid[node.vals[0]][index])
+                    val += 1
+                    grid[node.vals[0]][index] = val
+                    index -= 1
+
+            else:
+                index = node.vals[0]
+                while index <= node.vals[2]:
+                    val = int(grid[node.vals[0][index]])
+                    val += 1
+                    grid[node.vals[0][index]] = val
+                    index += 1
+
+
+    for y in range(len(grid)):
+        print(grid[y])
+
 def calculate_lines(coordinate_nodes):
     #Only check lines that are not diagonal
     nodes = []
@@ -31,23 +71,44 @@ def calculate_lines(coordinate_nodes):
         if n.vals[0] == n.vals[2] or n.vals[1] == n.vals[3]:
             nodes.append(n)
 
+    for node in nodes:
+        print(node)
     return nodes
-
 
 
 def create_coordinates_nodes(lines):
     coordinate_nodes = []
     for line in lines:
         vals = line.split(',')
-        start_x = vals[0]
-        start_y = vals[1]
-        end_x = vals[2]
-        end_y = vals[3]
+        start_x = int(vals[0])
+        start_y = int(vals[1])
+        end_x = int(vals[2])
+        end_y = int(vals[3])
         node = NodeLine()
         node.set_coordinates(start_x, start_y, end_x, end_y)
         coordinate_nodes.append(node)
 
     return coordinate_nodes
+
+
+def create_grid(lines):
+    max_X = 0
+    max_Y = 0
+    for line in lines:
+        vals = line.split(',')
+        val_1 = int(vals[0])
+        val_2 = int(vals[1])
+        max_val = max(val_1, val_2)
+        max_X = max(max_X, max_val)
+
+        val_3 = int(vals[2])
+        val_4 = int(vals[3])
+        max_val = max(val_3, val_4)
+        max_Y = max(max_Y, max_val)
+
+    grid = [[0 for i in range(max_Y)] for j in range(max_X)]
+
+    return grid, max_X, max_Y
 
 
 def read_input():
@@ -65,4 +126,6 @@ def read_input():
 if __name__ == '__main__':
     lines = read_input()
     coordinates = create_coordinates_nodes(lines)
+    grid, w, h = create_grid(lines)
     line_nodes = calculate_lines(coordinates)
+    populate_grid(line_nodes, grid, w, h)
